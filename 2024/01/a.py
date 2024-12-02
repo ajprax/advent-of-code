4702f821ec41b2d3eb16b6e83b476bd4d43870c1
Aaron Feldstein <ajprax@gmail.com>
2024-12-01 16:24:48 -0800

    2024 day 1

---
+++ 2024/01/a.py
@@ -0,0 +1,15 @@
+from utils import *
+
+
+def solve(input):
+    return (
+        split(input, "\n", "   ")
+        .unzip()
+        .map(lambda a: a.map(int).sorted())
+        .apply_and_fit(t(lambda l, r: zip(l, r)))
+        .map(t(difference))
+        .apply(sum)
+    )
+
+with print_duration():
+    print("solution:", solve(read("input.txt")))
---
+++ 2024/01/b.py
@@ -0,0 +1,16 @@
+from utils import *
+
+
+def solve(input):
+
+    left, right = (
+        split(input, "\n", "   ")
+        .unzip()
+        .map(lambda a: a.map(int))
+    )
+    counts = right.count()
+    return left.map(lambda l: l * counts.get(l, 0)).apply(sum)
+
+
+with print_duration():
+    print("solution:", solve(read("input.txt")))
