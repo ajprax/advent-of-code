e72c5c0be15a8e6f904d9fa8ab00ce84993defdc
Aaron Feldstein <ajprax@gmail.com>
2024-12-10 11:48:06 -0800

    2024 day 10

---
+++ 2024/10/b.py
@@ -0,0 +1,24 @@
+from utils import *
+
+
+def count_trails(map):
+    def _count_trails(x, y):
+        locations = List([(x, y)])
+        for h in range(1, 10):
+            locations = (
+                locations
+                .flat_map(t(neighbors))
+                .filter(t(inbounds(map)))
+                .filter(t(lambda x, y: map[y][x] == h))
+            )
+        return locations.size()
+    return _count_trails
+
+
+def solve(input):
+    map = split(input, "\n", "").map(lambda row: row.map(int))
+    return indices(map).filter(t(lambda x, y: map[y][x] == 0)).map(t(count_trails(map))).apply(sum)
+
+
+with print_duration():
+    print("solution:", solve(read("input.txt")))
